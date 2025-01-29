a5963aea0fbc7545eb2ad9479c67331abd6cb1c8
Thomas LE <thole@paul-f5Ar3s8.clusters.42paris.fr>
2025-01-29 15:54:33 +0100

    test setup

--- setup.sh
+++ setup.sh
@@ -9,16 +9,16 @@ sudo apt-get install -y curl wget vim zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Installing virtual box
-wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O /usr/share/keyrings/oracle.asc
-gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys A2F683C52980AECF
-gpg --armor --export A2F683C52980AECF | sudo tee /usr/share/keyrings/oracle.asc
+sudo wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O /usr/share/keyrings/oracle.asc
+sudo gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys A2F683C52980AECF
+sudo gpg --armor --export A2F683C52980AECF | sudo tee /usr/share/keyrings/oracle.asc
 sudo echo 'deb [signed-by=/usr/share/keyrings/oracle.asc] http://download.virtualbox.org/virtualbox/debian bookworm contrib' >> /etc/apt/sources.list.d/virtualbox.list
 sudo apt-get update && sudo apt-get install virtualbox-7.1
 
 
 # Installing vagrant
-wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt-get update && sudo apt-get install -y vagrant
 
 
