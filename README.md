6b4db77cc746d46916a3d46520e52f5e0855e29c
Thomas LE <thole@paul-f5Ar3s8.clusters.42paris.fr>
2025-01-29 16:18:49 +0100

    finished setup script

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # auto_setup_IoT
 
-This is used to install the necessary dependencies. You should at least have git installed and have your user in your sudoers.
+This is used to install the necessary dependencies for IoT. You should at least have git installed and have your user in your sudoers.
+
--- setup.sh
+++ setup.sh
@@ -1,30 +1,40 @@
 #!/bin/sh
 
-sudo apt-get update
-sudo apt-get upgrade
+
+# Become root and give your user sudoers rights
+# su
+# sudo adduser $USER sudo
+# exit
+
+
+# install basic stuff
+sudo apt-get update -y
+sudo apt-get upgrade -y
 sudo apt-get install -y curl wget vim zsh
 
-
-# Installing oh-my-zsh (not necessary)
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Installing virtual box
 sudo wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O /usr/share/keyrings/oracle.asc
 sudo gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys A2F683C52980AECF
 sudo gpg --armor --export A2F683C52980AECF | sudo tee /usr/share/keyrings/oracle.asc
 echo 'deb [signed-by=/usr/share/keyrings/oracle.asc] http://download.virtualbox.org/virtualbox/debian bookworm contrib' | sudo tee -a /etc/apt/sources.list.d/virtualbox.list > /dev/null
-sudo apt-get update && sudo apt-get install virtualbox-7.1
+sudo apt-get update -y && sudo apt-get install -y virtualbox-7.1
 
 
 # Installing vagrant
 sudo wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt-get update && sudo apt-get install -y vagrant
+sudo apt-get update -y && sudo apt-get install -y vagrant
 
 
-# Installing vscode (not necessary)
+# Installing vscode (not necessary and NOT TESTED BE CAREFUL)
 # curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 # sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/keyrings/microsoft-archive-keyring.gpg
 # sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-# sudo apt-get update
-# sudo apt-get install code
+# sudo apt-get update -y
+# sudo apt-get install -y code
+
+
+# Installing oh-my-zsh (not necessary)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+chsh -s $(which zsh) $USER
