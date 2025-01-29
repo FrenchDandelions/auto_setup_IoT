e09ed5ed6ecaab4d78a67c7291e25bf49360895c
Thomas LE <thole@paul-f5Ar3s8.clusters.42paris.fr>
2025-01-29 15:47:38 +0100

    test setup

--- setup.sh
+++ setup.sh
@@ -2,10 +2,13 @@
 
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install -y curl wget
+sudo apt-get install -y curl wget vim zsh
 
 
-# installing virtual box
+# Installing oh-my-zsh (not necessary)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Installing virtual box
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O /usr/share/keyrings/oracle.asc
 gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys A2F683C52980AECF
 gpg --armor --export A2F683C52980AECF | sudo tee /usr/share/keyrings/oracle.asc
@@ -16,8 +19,10 @@ sudo apt-get update && sudo apt-get install virtualbox-7.1
 # Installing vagrant
 wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install vagrant
+sudo apt-get update && sudo apt-get install -y vagrant
 
+
+# Installing vscode (not necessary)
 # curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 # sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/keyrings/microsoft-archive-keyring.gpg
 # sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
